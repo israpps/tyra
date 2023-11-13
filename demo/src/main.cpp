6dd8a60c729e70460b9ac893800b7c9edf66d4d3
israpps <57065102+israpps@users.noreply.github.com>
2023-11-13 12:08:20 -0300

    add continous integration

--- demo/src/main.cpp
+++ demo/src/main.cpp
@@ -16,7 +16,7 @@ int main() {
   Tyra::EngineOptions options;
 
   if (Demo::IS_REAL_PS2_VIA_USB) {
-    options.loggingMode = LOGGING_FILE;
+    options.loggingMode = LOGGING_EESIO;
     options.loadUsbDriver = true;
   }
 
